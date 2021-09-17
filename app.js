5112c75d4a709c3b03d2e635fa7f005c298712f3
Roberto Paz <fsjedi@outlook.com>
2021-09-17 09:42:45 -0400

    primeras lineas

---
+++ app.js
@@ -0,0 +1,44 @@
+let userName = '';
+userName ? console.log(`Hello ${userName}!`) : console.log('Hello!');
+
+const userQuestion = 'Lloverá hoy?';
+
+console.log(`El jugador preguntó: ${userQuestion}`);
+
+const randomNumber = Math.floor(Math.random()*8);
+
+eightBall = ''
+
+const food = 'salad';
+ 
+switch (randomNumber) {
+  case 1 :
+    console.log('It is certain');
+    break;
+  case 2 :
+    console.log('It is decidedly so');
+    break;
+  case 3 :
+    console.log('Reply hazy try again');
+
+
+
+    default:
+}
+ 
+// Prints: Enjoy your meal
+
+const food = 'salad';
+ 
+switch (food) {
+  case 'oyster':
+    console.log('The taste of the sea 🦪');
+    break;
+  case 'pizza':
+    console.log('A delicious pie 🍕');
+    break;
+  default:
+    console.log('Enjoy your meal');
+}
+ 
+// Prints: Enjoy your meal
